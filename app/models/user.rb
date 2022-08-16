55d80cd1ee54a0bd81e65595bc84ec6c5da54935
Ranjithrajd <ranjithraj.d@gmail.com>
2022-08-16 23:36:06 +0530

    user fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   has_many :productwatcher
   has_one :cart
 
-  validates :name, presence: true
   validates :email, presence: true
   validates :password, presence: true
 
