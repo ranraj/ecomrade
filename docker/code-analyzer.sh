0b194c8566c37458c6e8260156c6b44eb04f2c2e
Ranjithrajd <ranjithraj.d@gmail.com>
2022-08-17 11:26:58 +0530

    brakeman

---
+++ docker/code-analyzer.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+# static code analyser
+bundle exec brakeman:run 
+bundle exec rubocop
--- docker/startup.sh
+++ docker/startup.sh
@@ -2,4 +2,5 @@
 
 docker/wait-for-services.sh
 docker/prepare-db.sh
+#docker/code-analyzer.sh
 bundle exec puma -C ./config/puma.rb
