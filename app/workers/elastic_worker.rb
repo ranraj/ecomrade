b88277267fe2dfd094153c7128a9eb87d2f8df8f
Ranjithrajd <ranjithraj.d@gmail.com>
2023-06-28 15:05:14 +0530

    Update elastic_worker.rb

--- app/workers/elastic_worker.rb
+++ app/workers/elastic_worker.rb
@@ -8,7 +8,7 @@ class ElasticWorker
     id = args[0]
     klass = args[1]
     object = klass.constantize.find(id.to_s).reindex
-    Rails.logger "ElasticWorker Synced #{object}"
+    Rails.logger "ElasticWorker Synced #{object} .."
   rescue StandardError => e
     Rails.logger "ElasticWorker Sync failure: #{e.message}"
   end
